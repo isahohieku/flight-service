a08db8ab96093dd119bd4a6a888339318f6fce1f
Isah Ohieku <isahohieku@gmail.com>
2019-12-25 17:36:32 +0100

    added routes file in the auth model

---
+++ src/app/auth/routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: SigninComponent
+    },
+];
+
+export { routes };
