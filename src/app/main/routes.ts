04018ebc0b5bd285f02faa2db392e8cc589bcd72
Isah Ohieku <isahohieku@gmail.com>
2019-12-25 18:30:07 +0100

    added a layout module ad main routes registered

--- src/app/main/routes.ts
+++ src/app/main/routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from '../layout/layout.component';
+// import { LayoutComponent } from '../layout/layout.component';
 import { AuthComponent } from '../auth/auth.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-            { path: '', component: AuthComponent, loadChildren: '../core/core.module#CoreModule' },
-        ]
-    }
+    { path: '', component: AuthComponent, loadChildren: '../core/core.module#CoreModule' }
 ];
