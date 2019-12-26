573ded269e6cd1603860576734765677dd40005b
Isah Ohieku <isahohieku@gmail.com>
2019-12-26 13:40:42 +0100

    corrected deployment directory

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const app = express();
 
 app.use(compression());
 
-app.use(express.static(__dirname + '/dist/business-directory'));
+app.use(express.static(__dirname + '/dist/flights'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/business-directory/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/flights/index.html'));
 })
 
 app.listen(process.env.PORT || 8080);
